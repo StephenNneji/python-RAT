8443c124383bd53c59f540b98499450919f9ee94
Stephen Nneji <steve.nneji@gmail.com>
2026-07-16 11:28:20 +0100

    Address review comment

--- ratapi/inputs.py
+++ ratapi/inputs.py
@@ -244,9 +244,8 @@ def make_problem(project: ratapi.Project) -> ProblemDefinition:
 
         if background.type == TypeOptions.Data:
             # Background data is appended to contrast data so empty index
-            contrast_background_param.append(np.array([]))
             if background.value_1 != "":
-                contrast_background_param[-1] = project.background_parameters.index(background.value_1)
+                contrast_background_param.append(project.background_parameters.index(background.value_1))
             # If we are using a data background, we add the background data to the contrast data
             data = append_data_background(data, project.data[background.source].data)
 
@@ -504,11 +503,11 @@ def check_indices(problem: ProblemDefinition) -> None:
         param_list = getattr(problem, source_param_lists[background_type])
         # For data, background data is appended to the contrast data so first index could
         # either be empty or be the index of an offset.
-        first_index = background_data[0]
-        if np.size(first_index) > 0 and not 0 < first_index <= len(param_list):
+        if background_data and not 0 < background_data[0] <= len(param_list):
+            first_entry_name = "data offset" if background_type == "data" else "source"
             raise IndexError(
                 f'Entry {i} of contrastBackgroundParams has type "{background_type}" '
-                f"and source index {first_index}, "
+                f"and {first_entry_name} index {background_data[0]}, "
                 f'which is outside the range of "{source_param_lists[background_type]}".'
             )
 
